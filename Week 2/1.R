981dcdc08f56d8c7f42bbbbb3d65f74a3117a1b5
Pranit Kotkar <69496273+pranitkotkar@users.noreply.github.com>
2020-10-07 21:51:21 +0530

    Create 1.R

---
+++ Week 2/1.R
@@ -0,0 +1,34 @@
+#install.packages("jsonlite")
+library(jsonlite)
+#install.packages("httpuv")
+library(httpuv)
+#install.packages("httr")
+library(httr)
+
+# Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "Coursera_John_Hopkins",
+                   key = "7c6b9f19628fe7e0b0a8",
+                   secret = "3cafbba23cdc68bdf1344a5afe0e644687045808")
+
+# Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+
